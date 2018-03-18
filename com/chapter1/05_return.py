ebef57bfb86791fd29fa40c1cf13b7602ec95930
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-03-18 12:14:05 +0900

    return 예제 수정

--- com/chapter1/05_return.py
+++ com/chapter1/05_return.py
@@ -1,11 +1,7 @@
 def plus(val1, val2):
     return val1 + val2
 
-plus2 = lambda x, y: x + y
-
 result = plus(10, 20)
-result2 = plus2(20, 30)
 
 print(result)
-print(result2)
 
--- com/crawl/01_url_open.py
+++ com/crawl/01_url_open.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("http://www.google.com")
+html = urlopen("http://www.naver.com")
 bsObj = BeautifulSoup(html.read(), "html.parser")
 print(bsObj)
 
